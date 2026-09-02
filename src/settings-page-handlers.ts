f536b7c86cc0d8f85315a4047662d8be4651c71a
marinski1112 <marinski1112@gmail.com>
2026-09-02 13:17:09 +0900

    Route settings notifications through retained page

--- src/settings-page-handlers.ts
+++ src/settings-page-handlers.ts
@@ -1,3 +1,4 @@
-export { settings, settingsDiagnostics, settingsNotifications, recurring } from './app';
+export { settings, settingsDiagnostics, recurring } from './app';
 export { settingsContent } from './settings-content-page';
 export { settingsMembers } from './settings-members-page';
+export { settingsNotifications } from './settings-notifications-page';
