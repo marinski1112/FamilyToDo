b7fe8c3d2263d73c5d27f6bfd992039b4232dcaf
marinski1112 <marinski1112@gmail.com>
2026-09-02 19:42:23 +0900

    Route item edit through retained module

--- src/task-page-handlers.ts
+++ src/task-page-handlers.ts
@@ -1,4 +1,5 @@
-export { taskEdit, itemEdit } from './app';
+export { taskEdit } from './app';
+export { itemEdit } from './item-edit-page';
 export { today, tomorrow } from './daily-task-page';
 export { taskEvents } from './task-events-page';
 export { taskView } from './task-view-page';
